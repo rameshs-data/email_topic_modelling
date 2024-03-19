9b14b12896ab5fb4c415ee64daf1e5d7ba5b3e34
Ramesh Suragam <ramesh.suragam@gmail.com>
2024-03-19 15:33:05 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this project is to use topic modelling techiques to categorise emails into categories',
+    author='Ramesh S',
+    license='MIT',
+)
